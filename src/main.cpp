c6889d6b2a7718e0db5a4681e88435b3eb036bbc
sohyunio <daegamee@yonsei.ac.kr>
2024-12-06 01:21:01 +0900

    add class skill

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,19 @@ const char FIRE = 5;
 // type naes as strings
 string type[6] = { "Normal", "Ground", "Electric", "Water", "Grass", "Fire" };
 
+// define skill
+class skill {
+public:
+	string name;
+	int type;
+	int damage;
+	int maxTry;
+	int count = maxTry;
+	skill() : name(""), type(0), damage(0), maxTry(0), count(0) {} // default constructor
+	skill(string n, int t, int d, int m)  // parameterized constructor
+		: name(n), type(t), damage(d), maxTry(m), count(m) {}
+};
+
 int main(){
 	return 0;
 	}
