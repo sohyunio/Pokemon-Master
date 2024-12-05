0938c7c3cf509819172fca1e9bfe3b6cc43547d3
sohyunio <daegamee@yonsei.ac.kr>
2024-12-06 01:18:17 +0900

    define skill

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,26 @@
-#include <iostream>
+﻿#include <iostream>
+#include <string>
 using namespace std;
 
+// interaction effecs in type
+const int TYPE[6][6] =
+{ {0, 0, 0, 0, 0, 0},
+	{0, 0, 5, 0,-3, 5},
+	{0,-3,-3, 5,-3, 0},
+	{0, 5, 0,-3,-3, 5},
+	{0,-3, 0, 5,-3,-3},
+	{0, 0, 0,-3, 5,-3} };
+	
+// type const
+const char NORMAL = 0;
+const char GROUND = 1;
+const char ELECTRIC = 2;
+const char WATER = 3;
+const char GRASS = 4;
+const char FIRE = 5;
+// type naes as strings
+string type[6] = { "Normal", "Ground", "Electric", "Water", "Grass", "Fire" };
+
 int main(){
-        cout << "Hello, World!" << endl;
-        return 0;
-}
+	return 0;
+	}
